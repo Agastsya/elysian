76c79f8e8f6b136a8e9803380483ba0f26b491d9
agastsya <agastsya007@gmail.com>
2023-08-08 23:04:26 +0530

    Structure of project developed

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import mainlogo from "../assets/stumbleupon.png";
+
+const About = () => {
+  return (
+    <>
+      <img src={mainlogo} alt="" className="h-15" />
+    </>
+  );
+};
+
+export default About;
