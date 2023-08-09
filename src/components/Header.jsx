1e7d57e0abef5d8c60b7488f936962ac4a1bac55
agastsya <agastsya007@gmail.com>
2023-08-10 03:04:32 +0530

    making the homepage much better

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,105 @@
 import { NavLink } from "react-router-dom";
-import { HStack, VStack, Stack, Container, Text } from "@chakra-ui/react";
+import { Stack, Text, Center, Box } from "@chakra-ui/react";
+import { GiFlowerPot } from "react-icons/gi";
+import { BsSearch, BsBasket } from "react-icons/bs";
 
 const Header = () => {
   return (
     <>
+      <Center h={"27"} color={"white"} bgColor={"#38A169"}>
+        {" "}
+        Find Your New Favorite Product
+      </Center>
       <Stack
         direction={"row"}
         w={"xl"}
         justifyContent={"space-between"}
         px={"16"}
         py={"12"}
+        className=" border-spacing-0 border-b-2 "
       >
-        <Text>The Muse</Text>
-        <ul className="flex gap-5">
+        <Center className="text-xl font-extrabold underline">
+          The Elysian
+        </Center>
+        <ul className="flex gap-16">
           <li>
-            <NavLink to={"/resources"}>
-              {" "}
-              <VStack w={"20"} align={"flex-start"}>
-                <Text>Resources</Text>
-                <Text>FAQ,Filing,Support</Text>
-              </VStack>
+            <NavLink to={"/resources"} className=" flex">
+              <GiFlowerPot size={"42"} className="mr-4 mt-1" />
+              <Box className="flex-col">
+                <Text
+                  fontWeight={"bold"}
+                  fontSize={"lg"}
+                  display={"flex"}
+                  top={"10"}
+                >
+                  Resources
+                </Text>
+                <Text as={"i"} color={"gray"}>
+                  FAQ,Filing,Support
+                </Text>
+              </Box>
             </NavLink>
           </li>
+
           <li>
             <NavLink to={"/visit"}>
               {" "}
-              <VStack w={"20"} align={"flex-start"}>
-                <Text>Visit the Elysian</Text>
-                <Text>Store Locator</Text>
-              </VStack>
+              <Box w={"20"} align={"flex-col"}>
+                <Text
+                  fontWeight={"bold"}
+                  fontSize={"lg"}
+                  display={"flex"}
+                  top={"10"}
+                >
+                  Visit the Elysian
+                </Text>
+                <Text as={"i"} color={"gray"}>
+                  Store Locator
+                </Text>
+              </Box>
             </NavLink>
           </li>
+
           <li>
             <NavLink to={"/signin"}>
               {" "}
-              <VStack w={"20"} align={"flex-start"}>
-                <Text>Hello,Sign In</Text>
-                <Text>Your Account</Text>
-              </VStack>
+              <Box w={"20"} align={"flex-col"}>
+                <Text
+                  fontWeight={"bold"}
+                  fontSize={"lg"}
+                  display={"flex"}
+                  top={"10"}
+                >
+                  Hello,Sign In
+                </Text>
+                <Text as={"i"} color={"gray"}>
+                  Your Account
+                </Text>
+              </Box>
             </NavLink>
           </li>
-          <li></li>
+
+          <li>
+            <NavLink to={"/search"}>
+              <BsSearch size={"27"} className="my-3" />
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to={"/cart"}>
+              <BsBasket size={"27"} className="my-3 mr-16 " />
+            </NavLink>
+          </li>
         </ul>
       </Stack>
+      <Center gap={"80"} bgColor={"#EDF2F7"} h={"40"}>
+        <NavLink to={"/newarrivals"}>NEW ARRIVALS</NavLink>
+        <NavLink to={"/men"}>MEN</NavLink>
+        <NavLink to={"/women"}>WOMEN</NavLink>
+        <NavLink to={"/kids"}>KDIS</NavLink>
+        <NavLink to={"/accessories"}>ACCESSORIES</NavLink>
+        <NavLink to={"/giftideas"}>GIFT IDEAS</NavLink>
+      </Center>
     </>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,31 @@
+import {
+  HStack,
+  Box,
+  VStack,
+  Stack,
+  Center,
+  AbsoluteCenter,
+  Text,
+} from "@chakra-ui/react";
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <Stack bgColor={"#9AE6B4"}>
+        <VStack width={"50%"} h={"80vh"} className="relative">
+          <Box alignContent={"flex-start"} className="absolute" top={"40%"}>
+            <Text size={"xl"} fontFamily={"sans-serif"} className="text-5xl">
+              YOUR SUMMER WARDROBE
+            </Text>
+            <Text className="w-2/3 font-semibold">
+              Enjoy the feel-good vibes of season with our easy-care, highly
+              durable clothes and become an elysian.
+            </Text>
+          </Box>
+        </VStack>
+        <VStack></VStack>
+      </Stack>
+    </>
+  );
 };
 
 export default Home;
